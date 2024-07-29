64007238c785ac3ad4fc274a72f940354235087f
blank <aapeli.saarelainen.76@gmail.com>
2024-07-29 16:26:30 +0300

    added some functionality

--- src/vector3.zig
+++ src/vector3.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const math = std.math;
 const constants = @import("constants.zig");
 const quat = @import("quaternion.zig");
 
@@ -81,26 +82,6 @@ pub inline fn dataCreate(data: @Vector(3, f32)) Vector3 {
     };
 }
 
-pub inline fn dot(vec1: Vector3, vec2: Vector3) f32 {
-    return vec1.data[0] * vec2.data[0] + vec1.data[1] * vec2.data[1] + vec1.data[2] * vec2.data[2];
-}
-
-pub inline fn dotData(vec1: @Vector(3, f32), vec2: @Vector(3, f32)) f32 {
-    return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2];
-}
-
-pub inline fn cross(vec1: Vector3, vec2: Vector3) Vector3 {
-    return .{ .data = @Vector(3, f32){ (vec1.data[1] * vec2.data[2] - vec1.data[2] * vec2.data[1]), (vec1.data[2] * vec2.data[0] - vec1.data[0] * vec2.data[2]), (vec1.data[0] * vec2.data[1] - vec1.data[1] * vec2.data[0]) } };
-}
-
-pub inline fn crossData(vec1: @Vector(3, f32), vec2: @Vector(3, f32)) @Vector(3, f32) {
-    return @Vector(3, f32){ (vec1[1] * vec2[2] - vec1[2] * vec2[1]), (vec1[2] * vec2[0] - vec1[0] * vec2[2]), (vec1[0] * vec2[1] - vec1[1] * vec2[0]) };
-}
-
-pub inline fn rotate(vec1: Vector3, rot: quat.Quaternion) Vector3 {
-    vec1.data = vec1.data + ((crossData(rot.data[1..4], vec1.data) * rot[0]) + (crossData(rot.data[1..4], (crossData(rot.data[1..4], vec1.data))))) * 2.0;
-}
-
 pub inline fn add(vec1: Vector3, vec2: Vector3) Vector3 {
     return .{
         .data = vec1.data + vec2.data,
@@ -141,4 +122,32 @@ pub inline fn negate(vec1: Vector3) Vector3 {
     return .{
         .data = -vec1.data,
     };
+}
+
+pub inline fn dot(vec1: Vector3, vec2: Vector3) f32 {
+    return vec1.data[0] * vec2.data[0] + vec1.data[1] * vec2.data[1] + vec1.data[2] * vec2.data[2];
+}
+
+pub inline fn dotData(vec1: @Vector(3, f32), vec2: @Vector(3, f32)) f32 {
+    return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2];
+}
+
+pub inline fn cross(vec1: Vector3, vec2: Vector3) Vector3 {
+    return .{ .data = @Vector(3, f32){ (vec1.data[1] * vec2.data[2] - vec1.data[2] * vec2.data[1]), (vec1.data[2] * vec2.data[0] - vec1.data[0] * vec2.data[2]), (vec1.data[0] * vec2.data[1] - vec1.data[1] * vec2.data[0]) } };
+}
+
+pub inline fn crossData(vec1: @Vector(3, f32), vec2: @Vector(3, f32)) @Vector(3, f32) {
+    return @Vector(3, f32){ (vec1[1] * vec2[2] - vec1[2] * vec2[1]), (vec1[2] * vec2[0] - vec1[0] * vec2[2]), (vec1[0] * vec2[1] - vec1[1] * vec2[0]) };
+}
+
+pub inline fn rotate(vec1: Vector3, rot: quat.Quaternion) Vector3 {
+    vec1.data = vec1.data + ((crossData(rot.data[1..4], vec1.data) * rot[0]) + (crossData(rot.data[1..4], (crossData(rot.data[1..4], vec1.data))))) * 2.0;
+}
+
+pub inline fn length(vec1: Vector3) f32 {
+    return @sqrt(math.pow(f32, vec1.data[0], 2.0) + math.pow(f32, vec1.data[1], 2.0) + math.pow(f32, vec1.data[2], 2.0));
+}
+
+pub inline fn distance(vec1: Vector3, vec2: Vector3) f32 {
+    return @sqrt(math.pow(f32, vec2.data[0] - vec1.data[0], 2.0) + math.pow(f32, vec2.data[1] - vec1.data[1], 2.0) + math.pow(f32, vec2.data[2] - vec1.data[2], 2.0));
 }
