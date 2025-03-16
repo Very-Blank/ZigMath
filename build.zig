c4f4f4e151974ce02037b98225364e30e5224892
Very-Blank <aapeli.saarelainen.76@gmail.com>
2025-03-16 09:56:00 +0200

    fix: Build.zig was incorrect

--- build.zig
+++ build.zig
@@ -4,7 +4,8 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const main_mod = b.createModule(.{
+    // tests
+    const testMod = b.createModule(.{
         .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
@@ -16,22 +17,29 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    main_mod.addImport("panictest", panic_test);
-
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "ZigMath",
-        .root_module = main_mod,
-    });
-
-    b.installArtifact(lib);
+    testMod.addImport("panictest", panic_test);
 
     const unit_tests = b.addTest(.{
-        .root_module = main_mod,
+        .root_module = testMod,
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+
+    // library
+    const libMod = b.createModule(.{
+        .root_source_file = b.path("src/zigmath.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "ZigMath",
+        .root_module = libMod,
+    });
+
+    b.installArtifact(lib);
 }
