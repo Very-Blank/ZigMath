f6f600ac1c7e3aec5374ab2ce2c596bb657f7528
Very-Blank <aapeli.saarelainen.76@gmail.com>
2025-03-17 08:37:45 +0200

    More fixes, rotating vectors might be wrong.

--- src/quaternion.zig
+++ src/quaternion.zig
@@ -1,78 +1,85 @@
 const constants = @import("constants.zig");
 const Vector3 = @import("vector3.zig").Vector3;
 
-pub const Quaternion = struct {
-    fields: @Vector(4, f32),
+pub fn Quaternion(comptime T: type) type {
+    switch (@typeInfo(T)) {
+        .float, .comptime_float => {},
+        else => @compileError("Type not supported. Was given " ++ @typeName(T) ++ " type."),
+    }
 
-    pub fn setFromAngle(radians: f32, axis: constants.Axis) void {
-        switch (axis) {
-            .x => {
-                return .{
-                    .fields = @Vector(4, f32){ @cos(radians / 2), @sin(radians / 2), 0, 0 },
-                };
-            },
-            .y => {
-                return .{
-                    .fields = @Vector(4, f32){ @cos(radians / 2), 0, @sin(radians / 2), 0 },
-                };
-            },
-            .z => {
-                return .{
-                    .fields = @Vector(4, f32){ @cos(radians / 2), 0, 0, @sin(radians / 2) },
-                };
-            },
+    return struct {
+        fields: @Vector(4, T),
+
+        pub fn setFromAngle(radians: T, axis: constants.Axis) void {
+            switch (axis) {
+                .x => {
+                    return .{
+                        .fields = @Vector(4, T){ @cos(radians / 2), @sin(radians / 2), 0, 0 },
+                    };
+                },
+                .y => {
+                    return .{
+                        .fields = @Vector(4, T){ @cos(radians / 2), 0, @sin(radians / 2), 0 },
+                    };
+                },
+                .z => {
+                    return .{
+                        .fields = @Vector(4, T){ @cos(radians / 2), 0, 0, @sin(radians / 2) },
+                    };
+                },
+            }
         }
-    }
 
-    // Uses x, y, z order
-    pub fn setFromVector(self: *Quaternion, vector: Vector3) void {
-        self.setMultiply(multiply(createFromRadians(vector.x, constants.Axis.x), createFromRadians(vector.y, constants.Axis.y)), createFromRadians(vector.z, constants.Axis.z));
-    }
-
-    // Hamilton product
-    pub fn setMultiply(self: *Quaternion, other: Quaternion) void {
-        self.fields = @Vector(4, f32){
-            self.fields[0] * other.fields[0] - self.fields[1] * other.fields[1] - self.fields[2] * other.fields[2] - self.fields[3] * other.fields[3],
-            self.fields[0] * other.fields[1] + self.fields[1] * other.fields[0] + self.fields[2] * other.fields[3] - self.fields[3] * other.fields[2],
-            self.fields[0] * other.fields[2] - self.fields[1] * other.fields[3] + self.fields[2] * other.fields[0] + self.fields[3] * other.fields[1],
-            self.fields[0] * other.fields[3] + self.fields[1] * other.fields[2] - self.fields[2] * other.fields[1] + self.fields[3] * other.fields[0],
-        };
-    }
-
-    pub fn createFromRadians(radians: f32, axis: constants.Axis) Quaternion {
-        switch (axis) {
-            .x => {
-                return .{
-                    .fields = @Vector(4, f32){ @cos(radians / 2), @sin(radians / 2), 0, 0 },
-                };
-            },
-            .y => {
-                return .{
-                    .fields = @Vector(4, f32){ @cos(radians / 2), 0, @sin(radians / 2), 0 },
-                };
-            },
-            .z => {
-                return .{
-                    .fields = @Vector(4, f32){ @cos(radians / 2), 0, 0, @sin(radians / 2) },
-                };
-            },
+        // Uses x, y, z order
+        pub fn setFromVector(self: *Quaternion(T), vector: Vector3) void {
+            self.setMultiply(multiply(createFromRadians(vector.x, constants.Axis.x), createFromRadians(vector.y, constants.Axis.y)), createFromRadians(vector.z, constants.Axis.z));
         }
-    }
 
-    // Uses x, y, z order
-    pub fn createFromVector(vector: Vector3) Quaternion {
-        return multiply(multiply(createFromRadians(vector.x, constants.Axis.x), createFromRadians(vector.y, constants.Axis.y)), createFromRadians(vector.z, constants.Axis.z));
-    }
+        // Hamilton product
+        pub fn setMultiply(self: *Quaternion(T), other: Quaternion(T)) void {
+            self.fields = @Vector(4, T){
+                self.fields[0] * other.fields[0] - self.fields[1] * other.fields[1] - self.fields[2] * other.fields[2] - self.fields[3] * other.fields[3],
+                self.fields[0] * other.fields[1] + self.fields[1] * other.fields[0] + self.fields[2] * other.fields[3] - self.fields[3] * other.fields[2],
+                self.fields[0] * other.fields[2] - self.fields[1] * other.fields[3] + self.fields[2] * other.fields[0] + self.fields[3] * other.fields[1],
+                self.fields[0] * other.fields[3] + self.fields[1] * other.fields[2] - self.fields[2] * other.fields[1] + self.fields[3] * other.fields[0],
+            };
+        }
 
-    // Hamilton product
-    pub fn multiply(quat1: Quaternion, quat2: Quaternion) Quaternion {
-        return .{
-            .data = @Vector(4, f32){
-                quat1.fields[0] * quat2.fields[0] - quat1.fields[1] * quat2.fields[1] - quat1.fields[2] * quat2.fields[2] - quat1.fields[3] * quat2.fields[3],
-                quat1.fields[0] * quat2.fields[1] + quat1.fields[1] * quat2.fields[0] + quat1.fields[2] * quat2.fields[3] - quat1.fields[3] * quat2.fields[2],
-                quat1.fields[0] * quat2.fields[2] - quat1.fields[1] * quat2.fields[3] + quat1.fields[2] * quat2.fields[0] + quat1.fields[3] * quat2.fields[1],
-                quat1.fields[0] * quat2.fields[3] + quat1.fields[1] * quat2.fields[2] - quat1.fields[2] * quat2.fields[1] + quat1.fields[3] * quat2.fields[0],
-            },
-        };
-    }
-};
+        pub fn createFromRadians(radians: T, axis: constants.Axis) Quaternion(T) {
+            switch (axis) {
+                .x => {
+                    return .{
+                        .fields = @Vector(4, T){ @cos(radians / 2), @sin(radians / 2), 0, 0 },
+                    };
+                },
+                .y => {
+                    return .{
+                        .fields = @Vector(4, T){ @cos(radians / 2), 0, @sin(radians / 2), 0 },
+                    };
+                },
+                .z => {
+                    return .{
+                        .fields = @Vector(4, T){ @cos(radians / 2), 0, 0, @sin(radians / 2) },
+                    };
+                },
+            }
+        }
+
+        // Uses x, y, z order
+        pub fn createFromVector(vector: Vector3) Quaternion(T) {
+            return multiply(multiply(createFromRadians(vector.x, constants.Axis.x), createFromRadians(vector.y, constants.Axis.y)), createFromRadians(vector.z, constants.Axis.z));
+        }
+
+        // Hamilton product
+        pub fn multiply(quat1: Quaternion(T), quat2: Quaternion(T)) Quaternion(T) {
+            return .{
+                .data = @Vector(4, T){
+                    quat1.fields[0] * quat2.fields[0] - quat1.fields[1] * quat2.fields[1] - quat1.fields[2] * quat2.fields[2] - quat1.fields[3] * quat2.fields[3],
+                    quat1.fields[0] * quat2.fields[1] + quat1.fields[1] * quat2.fields[0] + quat1.fields[2] * quat2.fields[3] - quat1.fields[3] * quat2.fields[2],
+                    quat1.fields[0] * quat2.fields[2] - quat1.fields[1] * quat2.fields[3] + quat1.fields[2] * quat2.fields[0] + quat1.fields[3] * quat2.fields[1],
+                    quat1.fields[0] * quat2.fields[3] + quat1.fields[1] * quat2.fields[2] - quat1.fields[2] * quat2.fields[1] + quat1.fields[3] * quat2.fields[0],
+                },
+            };
+        }
+    };
+}
--- src/zigmath.zig
+++ src/zigmath.zig
@@ -1,6 +1,5 @@
 pub const Vector2 = @import("vector2.zig").Vector2;
-pub const Vec3 = @import("vector3.zig");
+pub const vec3 = @import("vector3.zig");
 pub const Mat4 = @import("mat4.zig").Mat4;
-pub const Quaternion = @import("quaternion.zig").Quaternion;
+pub const quat = @import("quaternion.zig");
 pub const Constants = @import("constants.zig");
-pub const std = @import("std");
