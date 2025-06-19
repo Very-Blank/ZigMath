1bb096ac147d540c80085ee3bbc9e6491d2db967
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-19 18:07:51 +0300

    fix: F32 types not public

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ pub const Mat4 = @import("mat4.zig").Mat4;
 pub const Quaternion = @import("quaternion.zig").Quaternion;
 pub const Axis = @import("quaternion.zig").Axis;
 
-const @"f32" = struct {
+pub const @"f32" = struct {
     pub const Vector2 = @import("vector2.zig").Vector2(f32);
     pub const Vector3 = @import("vector3.zig").Vector3(f32);
     pub const Mat4 = @import("mat4.zig").Mat4(f32);
