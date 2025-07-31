321e46edf851a1390c2ec786e534c91457b7b97a
very-blank <aapeli.saarelainen.76@gmail.com>
2025-07-31 18:30:39 +0300

    feat: Inverse and Determinant for mat4

--- src/mat4.zig
+++ src/mat4.zig
@@ -3,109 +3,6 @@ const Vector3 = @import("vector3.zig").Vector3;
 const quat = @import("quaternion.zig");
 const math = std.math;
 
-test "determinant" {
-    const mat1 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 1.0, 0.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 1.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 1.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 1.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(1.0, mat1.determinant(), 0.1);
-
-    const mat2 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 2.0, 0.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 2.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 2.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 2.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(16.0, mat2.determinant(), 0.1);
-
-    const mat3 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 1.0, 0.0, 0.0, 5.0 },
-            @Vector(4, f32){ 0.0, 1.0, 0.0, 3.0 },
-            @Vector(4, f32){ 0.0, 0.0, 1.0, -2.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 1.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(1.0, mat3.determinant(), 0.1);
-
-    const mat4 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 2.0, 0.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 3.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 4.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 1.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(24.0, mat4.determinant(), 0.1);
-
-    const mat5 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 2.0, 1.0, 3.0, 4.0 },
-            @Vector(4, f32){ 0.0, 3.0, 2.0, 1.0 },
-            @Vector(4, f32){ 0.0, 0.0, 4.0, 5.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 5.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(120.0, mat5.determinant(), 0.1);
-
-    const mat6 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 0.0, -1.0, 0.0, 0.0 },
-            @Vector(4, f32){ 1.0, 0.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 1.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 1.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(1.0, mat6.determinant(), 0.1);
-
-    const mat7 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 1.0, 0.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 1.0, 0.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, -1.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 1.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(-1.0, mat7.determinant(), 0.1);
-
-    const mat8 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 1.0, 2.0, 3.0, 4.0 },
-            @Vector(4, f32){ 2.0, 4.0, 6.0, 8.0 },
-            @Vector(4, f32){ 0.0, 0.0, 1.0, 0.0 },
-            @Vector(4, f32){ 0.0, 0.0, 0.0, 1.0 },
-        },
-    };
-
-    try std.testing.expectApproxEqRel(0.0, mat8.determinant(), 0.1);
-
-    const mat9 = Mat4(f32){
-        .fields = [4]@Vector(4, f32){
-            @Vector(4, f32){ 1.0, 2.0, 0.0, 3.0 },
-            @Vector(4, f32){ 4.0, 1.0, 2.0, 0.0 },
-            @Vector(4, f32){ 0.0, 3.0, 1.0, 4.0 },
-            @Vector(4, f32){ 2.0, 0.0, 4.0, 1.0 },
-        },
-    };
-
-    std.debug.print("{any}\n", .{mat9.determinant()});
-
-    try std.testing.expectApproxEqRel(-47.0, mat9.determinant(), 0.1);
-}
-
 pub fn Mat4(comptime T: type) type {
     switch (@typeInfo(T)) {
         .float, .comptime_float => {},
@@ -208,44 +105,53 @@ pub fn Mat4(comptime T: type) type {
             };
         }
 
-        // FIXME:
-        pub fn determinant(self: Self) T {
-            var det: T = 0;
-            inline for (0..4) |i| {
-                if (i % 2 == 0) {
-                    det += self.fields[0][i] * det3x3(
-                        [3]@Vector(3, T){
-                            @Vector(3, T){ self.fields[1][(i + 1) % 4], self.fields[1][(i + 2) % 4], self.fields[1][(i + 3) % 4] },
-                            @Vector(3, T){ self.fields[2][(i + 1) % 4], self.fields[2][(i + 2) % 4], self.fields[2][(i + 3) % 4] },
-                            @Vector(3, T){ self.fields[3][(i + 1) % 4], self.fields[3][(i + 2) % 4], self.fields[3][(i + 3) % 4] },
-                        },
-                    );
-                } else {
-                    det -= self.fields[0][i] * det3x3(
-                        [3]@Vector(3, T){
-                            @Vector(3, T){ self.fields[1][(i + 1) % 4], self.fields[1][(i + 2) % 4], self.fields[1][(i + 3) % 4] },
-                            @Vector(3, T){ self.fields[2][(i + 1) % 4], self.fields[2][(i + 2) % 4], self.fields[2][(i + 3) % 4] },
-                            @Vector(3, T){ self.fields[3][(i + 1) % 4], self.fields[3][(i + 2) % 4], self.fields[3][(i + 3) % 4] },
-                        },
-                    );
+        pub fn inverse(self: Self) Self {
+            const det = determinant(self);
+            std.debug.assert(det != 0);
+
+            for (0..4) |i| {
+                for (0..4) |j| {
+                    self.fields[i][j] /= det;
                 }
             }
 
-            return det;
+            return self;
+        }
+
+        pub fn determinant(self: Self) T {
+            return self.fields[0][0] * det3x3(
+                [3]@Vector(3, T){
+                    @Vector(3, T){ self.fields[1][1], self.fields[1][2], self.fields[1][3] },
+                    @Vector(3, T){ self.fields[2][1], self.fields[2][2], self.fields[2][3] },
+                    @Vector(3, T){ self.fields[3][1], self.fields[3][2], self.fields[3][3] },
+                },
+            ) - self.fields[0][1] * det3x3(
+                [3]@Vector(3, T){
+                    @Vector(3, T){ self.fields[1][0], self.fields[1][2], self.fields[1][3] },
+                    @Vector(3, T){ self.fields[2][0], self.fields[2][2], self.fields[2][3] },
+                    @Vector(3, T){ self.fields[3][0], self.fields[3][2], self.fields[3][3] },
+                },
+            ) + self.fields[0][2] * det3x3(
+                [3]@Vector(3, T){
+                    @Vector(3, T){ self.fields[1][0], self.fields[1][1], self.fields[1][3] },
+                    @Vector(3, T){ self.fields[2][0], self.fields[2][1], self.fields[2][3] },
+                    @Vector(3, T){ self.fields[3][0], self.fields[3][1], self.fields[3][3] },
+                },
+            ) - self.fields[0][3] * det3x3(
+                [3]@Vector(3, T){
+                    @Vector(3, T){ self.fields[1][0], self.fields[1][1], self.fields[1][2] },
+                    @Vector(3, T){ self.fields[2][0], self.fields[2][1], self.fields[2][2] },
+                    @Vector(3, T){ self.fields[3][0], self.fields[3][1], self.fields[3][2] },
+                },
+            );
         }
 
         inline fn det3x3(fields: [3]@Vector(3, T)) T {
-            var det: T = 0;
-
-            inline for (0..3) |i| {
-                if (i % 2 == 0) {
-                    det += fields[0][i] * (fields[1][(i + 1) % 3] * fields[2][(i + 2) % 3] - fields[1][(i + 2) % 3] * fields[2][(i + 1) % 3]);
-                } else {
-                    det -= fields[0][i] * (fields[1][(i + 1) % 3] * fields[2][(i + 2) % 3] - fields[1][(i + 2) % 3] * fields[2][(i + 1) % 3]);
-                }
-            }
-
-            return det;
+            // zig fmt: off
+            return fields[0][0] * (fields[1][1] * fields[2][2] - fields[1][2] * fields[2][1])
+                - fields[0][1] * (fields[1][0] * fields[2][2] - fields[1][2] * fields[2][0])
+                + fields[0][2] * (fields[1][0] * fields[2][1] - fields[1][1] * fields[2][0]);
+            // zig fmt: on
         }
 
         pub fn scale(sc: Vector3(T)) Self {
