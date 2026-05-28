21d49c7b10c8662d4456dcf37364d21cb6b08132
very-blank <aapeli.saarelainen.76@gmail.com>
2026-05-28 23:26:33 +0300

    fix: ) in the wrong place ......

--- src/vector3.zig
+++ src/vector3.zig
@@ -221,7 +221,7 @@ pub fn Vector3(comptime T: type, comptime Unique: type) type {
             std.debug.assert(length_squared != 0.0);
 
             const dot_product = @max(0.0, @min(1.0, dot(p.subtract(a), b.subtract(a)) / length_squared));
-            return a.add(b.subtract(a)).scale(dot_product);
+            return a.add(b.subtract(a).scale(dot_product));
         }
     };
 }
