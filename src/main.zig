b90d622c75e8a4da06a6635900426ca08b3cc4b8
very-blank <aapeli.saarelainen.76@gmail.com>
2026-06-06 22:21:53 +0300

    fix: Wrong import.

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ pub const Vector2 = @import("vector2.zig").Vector2;
 pub const Vector3 = @import("vector3.zig").Vector3;
 pub const Mat4 = @import("mat4.zig").Mat4;
 pub const Quaternion = @import("quaternion.zig").Quaternion;
-pub const AxisType = @import("quaternion.zig").AxisType;
+pub const AxisType = @import("axis.zig").AxisType;
 
 pub const @"f32" = struct {
     pub const Vector2 = @import("vector2.zig").Vector2(f32, opaque {});
